950346cd4c2c72fee48912545f2f4a5d6109e3c4
bunnypirate <bunnypirateprincess@gmail.com>
2016-12-14 21:26:51 -0700

    drill47

---
+++ drill47.py
@@ -0,0 +1,40 @@
+# Python: 2.7
+#
+# Author: Me
+#
+# Purpose: The Tech Academy - Python Course, Drill item 47
+
+#Start IDLE and use the Python range() function with one parameter to display the
+#following:
+#0
+#1
+#2
+#3
+for i in range(4):
+    print(i)
+
+#
+print ('\n')
+#
+
+#Use the Python range() function with 3 parameters to display the following:
+#3
+#2
+#1
+#0
+
+for j in range(3,-1,-1):
+    print(j)
+
+#
+print ('\n')
+#
+
+#Use the Python range() function with 3 parameters to display the following:
+#8
+#6
+#4
+#2
+
+for k in range(8,0,-2):
+    print(k)
